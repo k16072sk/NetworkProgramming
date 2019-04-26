0f52405ae28b6c2b44e7bc5dd81b5cddba493d59
4408_茅原 沙也 <k16072sk>
2019-04-26 10:23:05 +0900

    2019

--- client.c
+++ client.c
@@ -1,7 +1,18 @@
 #include<stdio.h>
 #include<arpa/inet.h>
+#include<unistd.h>
 #include<sys/socket.h>
+#include<string.h>
 
+void commun(int sock){
+	char buf[256];
+	int len_r;
+	char *message = "Hello World";
+	send(sock,message,strlen(message),0);
+	len_r = recv(sock,buf,256,0);
+	buf[len_r] = '\0';
+	printf("%s\n",buf);
+}
 int main(int argc,char**argv){
 	
 	int sock = socket(PF_INET,SOCK_STREAM,0);
@@ -15,6 +26,8 @@ int main(int argc,char**argv){
 	target.sin_port = htons(10001);
 	
 	connect(sock,(struct sockaddr*)&target,sizeof(target));
+	
+	commun(sock); /*メッセージのやり取りは関数にする*/
 	
 	close(sock);
 	return 0;
