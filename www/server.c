964be49d48bf53db58bd894ad14c3fbaed8931b0
k16072sk <k16072sk>
2019-06-21 10:13:01 +0900

    20190621

--- www/server.c
+++ www/server.c
@@ -15,10 +15,14 @@ void DieWithError(char *errorMessage){
 void commun(int sock){
 	char buf[BUF_SIZE];
 	int len_r;
+	char *message = "‚¨•Ô‚µ‚Å‚·!";
+	
 	if((len_r = recv(sock,buf,BUF_SIZE,0))<=0)
 		DieWithError("recv()filed");
+	
 	buf[len_r] = '\0';
 	printf("%s\n",buf);
+	
 	if(send(sock,buf,strlen(buf),0)!=strlen(buf))
 	DieWithError("send()sent a message of unexpected bytes");
 }
@@ -31,7 +35,7 @@ int main(int argc, char **argv){
 	
 	servAddress.sin_family = AF_INET;
 	servAddress.sin_addr.s_addr = htonl(INADDR_ANY);
-	servAddress.sin_port = htons(10001);
+	servAddress.sin_port = htons(80);
 	
 	bind(servSock,(struct sockaddr*)&servAddress,sizeof(servAddress));
 	listen(servSock,5);
@@ -40,6 +44,7 @@ int main(int argc, char **argv){
 		szClientAddr = sizeof(clientAddress);
 		cliSock = accept(servSock,(struct sockaddr*)&clientAddress,&szClientAddr);
 		commun(cliSock);
+		close(cliSock);
 	}
 	close(servSock);
 	return 0;
