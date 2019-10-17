865ae56ca9d25743fc10b0a95e7950a3d5b3c8a1
k16072sk <k16072sk@apps.kct.ac.jp>
2019-10-17 15:57:17 +0900

    2019/10/17

--- 3-message/client.c
+++ 3-message/client.c
@@ -48,3 +48,46 @@ int prepare_client_socket(char *ipaddr, int port)
 
     return sock;
 }
+
+void my_scanf(char *buf, int num_letter)
+{
+    char format[20];
+    sprintf(format, "%s$d%s", " ", num_letter, "s *[^\n]");
+    scanf(format, buf);
+    getchar();
+}
+
+void commun(int sock)
+{
+    char cmd[2] = "";
+    char withdraw[MONEY_DIGIT_SIZE + 1];
+    char deposit[MONEY_DIGIT_SIZE + 1];
+    char msg[BUF_SIZE];
+
+    printf("0:引き出し 1:預け入れ 2:残高照会 \n");
+    printf("何をしますか？ > ");
+
+    my_scanf(cmd, 1);
+
+    switch (cmd[0])
+    {
+    case '0':
+        pirntf("引き出す金額を入力してください\n");
+        my_scanf(withdraw, MONEY_DIGIT_SIZE);
+        sprintf(msg, "0_%s_", withdraw);
+        break;
+    case '1':
+        printf("預け入れる金額を入力してください\n");
+        my_scanf(deposit, MONEY_DIGIT_SIZE);
+        sprntf(msg, "%s_0_", deposit);
+        break;
+    case '2':
+        strcpy(msg, "0_0_");
+        break;
+    default:
+        printf("番号が確認できませんでした\n");
+        return;
+    }
+    if (send(sock, msg, strlen(msg), 0) != strlen(msg))
+        DieWithError("send() sent a message of unexpected bytes");
+}
