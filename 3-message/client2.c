e8df2e7aef5d1f555c5bb9691b6507e8a093840d
k16072sk <k16072sk@apps.kct.ac.jp>
2019-11-07 15:54:23 +0900

    master

--- 3-message/client2.c
+++ 3-message/client2.c
@@ -14,6 +14,13 @@ int prepare_client_socket(char *, int);
 void my_scanf(char *, int);
 void commun(int);
 void read_until_delim(int, char *, char, int);
+void read_certain_bytes(int, void *, int);
+
+struct money
+{
+    int deposit;
+    int withdraw;
+};
 
 int main(int argc, char *argv[])
 {
@@ -95,10 +102,10 @@ void read_until_delim(int sock, char *buf, char delimiter, int max_length)
 
 void commun(int sock)
 {
-    char cmd[2] = "";                    // コマンド入力用
-    char withdraw[MONEY_DIGIT_SIZE + 1]; // 引き出し額
-    char deposit[MONEY_DIGIT_SIZE + 1];  // 預け入れ額
-    char msg[BUF_SIZE];                  // 送信メッセージ
+    char cmd[2] = ""; // コマンド入力用
+    struct money msgMoney;
+    char money[BUF_SIZE]; // 送信メッセージ
+    int result;
 
     printf("0:引き出し　1:預け入れ　2:残高照会　9:終了\n");
     printf("何をしますか？ > ");
@@ -110,33 +117,48 @@ void commun(int sock)
     case '0':
         // 引き出し処理
         printf("引き出す金額を入力してください > ");
-        my_scanf(withdraw, MONEY_DIGIT_SIZE);
-
-        sprintf(msg, "0_%s_", withdraw);
+        my_scanf(money, MONEY_DIGIT_SIZE);
+        msgMoney.deposit = 0;
+        msgMoney.withdraw = atoi(money);
         break;
     case '1':
         //預け入れ処理
         printf("預け入れる金額を入力してください > ");
-        my_scanf(deposit, MONEY_DIGIT_SIZE);
-
-        sprintf(msg, "%s_0_", deposit);
+        my_scanf(money, MONEY_DIGIT_SIZE);
+        msgMoney.deposit = atoi(money);
+        msgMoney.withdraw = 0;
         break;
     case '2':
         // 残高照会
-        strcpy(msg, "0_0_");
+        msgMoney.deposit = 0;
+        msgMoney.withdraw = 0;
         break;
     default:
         // 終了
         printf("番号が確認できませんでした。\n");
         return;
     }
-    printf("%lu バイト\n", sizeof(char) * strlen(msg));
+    printf("%lu バイト\n", sizeof(msgMoney));
+
     // 送信処理
-    if (send(sock, msg, strlen(msg), 0) != strlen(msg))
+    if (send(sock, &msgMoney, sizeof(msgMoney), 0) != sizeof(msgMoney))
         DieWithError("send() sent a message of unexpected bytes");
 
     // 受信処理
-    read_until_delim(sock, msg, '_', BUF_SIZE);
+    read_certain_bytes(sock, &result, (int)sizeof(int));
     // 表示処理
-    printf("残高は%d円になりました", atoi(msg));
+    printf("残高は%d円になりました", result);
+}
+
+void read_certain_bytes(int sock, void *buf, int length)
+{
+    int len_r = 0;
+    int len_sum = 0;
+
+    while (len_sum < length)
+    {
+        if ((len_r = recv(sock, buf + len_sum, length - len_sum, 0)) <= 0)
+            DieWithError("recv() failed");
+        len_sum += len_r;
+    }
 }
